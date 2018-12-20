94226feeeb31f0192e82baf2fe7695d53af0dc8a
Ananda Umamil <zweimach@galatulis.io>
2018-12-20 19:27:53 +0700

    Add spreadsheet handler

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { Browser, Page, launch } from 'puppeteer';
-import * as xlsx from 'xlsx';
-import * as fs from 'fs';
+import { WorkBook, readFile } from 'xlsx';
+// import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
-
+/**
+ * Create Browser instance
+ *
+ * @async
+ *
+ * @returns {Browser} Returns a Browser instance from puppeteer
+ */
 async function openBrowser(): Promise<Browser> {
 	try {
 		return await launch({
@@ -18,6 +23,18 @@ async function openBrowser(): Promise<Browser> {
 }
 
 // tslint:disable:no-shadowed-variable
+/**
+ * Login to task URL
+ *
+ * @async
+ *
+ * @param {Page} browserPage Page object from Browser instance
+ * @param {string} loginUrl Login page URL
+ * @param {string} username Login username
+ * @param {string} password Login password
+ *
+ * @returns {Page} Returns a ready to use Page object
+ */
 async function loginPage(
 	browserPage: Page,
 	loginUrl: string,
@@ -26,18 +43,19 @@ async function loginPage(
 ): Promise<Page> {
 	try {
 		await browserPage.goto(loginUrl);
-		await browserPage.$$eval('input.form-control', input => {
-			(input[0] as HTMLInputElement).value = username;
-			(input[1] as HTMLInputElement).value = password;
-		});
-		// await browserPage.type('input.form-control', username);
-		// await browserPage.type('input.form-control', password);
+
+		await browserPage.waitFor('input[name=j_username]');
+		await browserPage.type('input[name=j_username]', username);
+		await browserPage.type('input[name=j_password]', password);
+
 		await Promise.all([
 			browserPage.waitForNavigation(),
 			browserPage.$eval('form#form1', form =>
 				(form as HTMLFormElement).submit()
 			)
 		]);
+		await browserPage.waitFor('#alert-Flag-2');
+		await browserPage.click('#alert-Flag-2');
 		return browserPage;
 	} catch (error) {
 		throw new Error(error.message);
@@ -47,21 +65,32 @@ async function loginPage(
 async function runQueries(taskUrl: string, browserPage: Page) {
 	try {
 		await browserPage.goto(taskUrl);
-		xlsx.readFile('list.xlsx');
-		await Promise.all([
-			browserPage.waitForNavigation(),
-			// @ts-ignore
-			browserPage.$eval('#loginform', form =>
-				(form as HTMLFormElement).submit()
-			)
-		]);
+		await browserPage.waitFor('#noba');
+		await browserPage.type('#noba', '12/test');
+		await browserPage.type('#tglba', '12-12-2018');
+		await browserPage.type('#npwp', '03.097.152.7-219.000');
+		await browserPage.click('#namaWP');
+		await browserPage.$eval(
+			'#alasan',
+			select => ((select as HTMLSelectElement).value = '5')
+		);
+		await browserPage.type('#nipAr', '198910062014021002');
+		await browserPage.click('#namaAr');
+		await browserPage.waitForNavigation();
 	} catch (error) {
 		// tslint:disable-next-line:no-console
-		fs.appendFile('log/error.log', error.message, () => console.log('Error'));
-		// throw new Error(error.message);
+		// fs.appendFile('log/error.log', error.message, () => console.log('Error'));
+		throw new Error(error.message);
 	}
 }
 
+/**
+ * Close Browser instance
+ *
+ * @async
+ *
+ * @param {Browser} browser Browser object from puppeteer
+ */
 async function closeBrowser(browser: Browser) {
 	try {
 		await browser.close();
@@ -70,17 +99,23 @@ async function closeBrowser(browser: Browser) {
 	}
 }
 
-async function runTasks(taskUrl: string, username: string, password: string) {
+async function runTasks(
+	loginUrl: string,
+	taskUrl: string,
+	username: string,
+	password: string
+) {
 	try {
 		const browser: Browser = await openBrowser();
 		const newPage: Page = await browser.newPage();
-		await newPage.setViewport({ width: 800, height: 480 });
+		await newPage.setViewport({ width: 1280, height: 720 });
 		const readyPage: Page = await loginPage(
 			newPage,
-			taskUrl,
+			loginUrl,
 			username,
 			password
 		);
+
 		await runQueries(taskUrl, readyPage);
 
 		await closeBrowser(browser);
@@ -89,9 +124,18 @@ async function runTasks(taskUrl: string, username: string, password: string) {
 	}
 }
 
-const username: string = '817932843';
-const password: string = 'Aichan99';
-// const username: string = (process.env.username as string) || '817932843';
-// const password: string = (process.env.password as string) || 'Aichan99';
+// Setup environment variables
+dotenv.config();
 
-runTasks('http://ereg/login', username, password);
+const username: string = process.env.USERNAME as string;
+const password: string = process.env.PASSWORD as string;
+
+// Setup xlsx workbook
+const workbook: WorkBook = readFile('data.xlsx');
+
+runTasks(
+	'https://ereg/login',
+	'https://ereg/dashboard/prosespermohonan/penetapannejabatan/',
+	username,
+	password
+);
